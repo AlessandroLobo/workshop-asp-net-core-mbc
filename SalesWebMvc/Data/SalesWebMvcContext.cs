811775f5ad159c284d56742bc659e9a2fd1a3694
Alessandro Lobo <alessandro.lobo@hotmail.com>
2022-10-31 17:18:26 -0300

    Seeding Service

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -15,8 +15,9 @@ namespace SalesWebMvc.Data
         }
 
         public DbSet<Department> Department { get; set; }
-        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<Seller> Seller { get; set; }
         public DbSet<SalesRecord> SalesRecord { get; set; }
+        
     }
 
 
