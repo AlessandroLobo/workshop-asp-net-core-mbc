8b762975918555a43aa4410c753ffc4ff729a4cd
Alessandro Lobo <alessandro.lobo@hotmail.com>
2022-10-31 19:55:12 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -16,6 +17,21 @@ namespace SalesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Add no banco de dados
+        [HttpPost]
+        // Valida token pra prevenir ataque de inserção
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
